35baa7ab429fb2e443bc8ec59bbd586c01234605
KazuhaM <kloenpo@gmail.com>
2019-10-10 11:00:37 +0900

    191010forExceptErr

--- 201904Mongolia/VegEro_Analysis.r
+++ 201904Mongolia/VegEro_Analysis.r
@@ -1,32 +1,34 @@
 path2 <- "D:/OneDrive - g.ecc.u-tokyo.ac.jp/LEP/2019/現行資料/0802春季モンゴル解析2/OriginalData"
 setwd(path2)
-d <- read.csv("Veg_EroAnalysis.csv", header =T)
+d <- read.csv("Veg_EroAnalysis2.csv", header =T)
 d
+
+pairs(d[,3:14])
 #群落タイプ名入力
 d$VegType[d$VegType== 1] <-"Bottom"
 d$VegType[d$VegType== 2] <-"SA(N)"
 d$VegType[d$VegType== 3] <-"SA(S)"
 d$VegType[d$VegType== 4] <-"Nirtaria"
 
-pairs(d[,3:14])
+
 
 #typeごとの植被率と群落高, Z0
 temp.type <- as.factor(d$VegType)
 boxplot(d$QuadCover~temp.type,xlab = "community type", ylab = "coverage(%)",
         main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
-dev.copy(pdf, file=paste("Type_Cov.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Type_Cov2.pdf",sep=""), width = 10, height = 10)
 dev.off()
 boxplot(d$QuadCommunityHeight~temp.type,xlab = "community type", ylab = "community height(m)",
         main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
-dev.copy(pdf, file=paste("Type_Hght.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Type_Hght2.pdf",sep=""), width = 10, height = 10)
 dev.off()
 boxplot(d$AveZ0~temp.type,xlab = "community type", ylab = "roughness length(m)",
         main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
-dev.copy(pdf, file=paste("Type_Z0.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Type_Z0.pdf2",sep=""), width = 10, height = 10)
 dev.off()
-boxplot(d$AveDev~temp.type,xlab = "community type", ylab = "average deviation of saltation flux between PC counter",
+boxplot(d$AveDev~temp.type,xlab = "community type", ylab = "NSDsf",
         main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
-dev.copy(pdf, file=paste("Type_Avedev.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Type_Avedev2.pdf",sep=""), width = 10, height = 10)
 dev.off()
 
 #粗度と臨界摩擦速度
@@ -34,21 +36,21 @@ plot(d$AveZ0,d$Ut,xlab = "roughness length(m)", ylab = "threshold friction veloc
      main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
 d.result <- lm(d$Ut~d$AveZ0)
 abline(d.result)
-text(0.05,50,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
+text(0.05,15,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
                         round(d.result$coefficients[2],digits = 2),"x\nR^2: ", 
                         round(summary(d.result)$r.squared,digits = 2) ,sep = ""))
-dev.copy(pdf, file=paste("Ust_Z0.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Ust_Z0.pdf2",sep=""), width = 10, height = 10)
 dev.off()
-
+summary(d.result)
 #coverと臨界摩擦速度
 plot(d$QuadCover,d$Ut,xlab = "Coverage", ylab = "threshold friction velocity(m/s)",
      main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
 d.result <- lm(d$Ut~d$QuadCover)
 abline(d.result)
-text(35,50,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
+text(35,15,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
                            round(d.result$coefficients[2],digits = 2),"x\nR^2: ", 
                            round(summary(d.result)$r.squared,digits = 2) ,sep = ""))
-dev.copy(pdf, file=paste("Ust_Cov.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Ust_Cov.pdf2",sep=""), width = 10, height = 10)
 dev.off()
 
 #heightと臨界摩擦速度
@@ -57,10 +59,10 @@ plot(temp.hght,d$Ut,xlab = "community height(m)", ylab = "threshold friction vel
      main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
 d.result <- lm(d$Ut ~ temp.hght)
 abline(d.result)
-text(0.12,50,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
+text(0.12,15,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
                          round(d.result$coefficients[2],digits = 2),"x\nR^2: ", 
                          round(summary(d.result)$r.squared,digits = 2) ,sep = ""))
-dev.copy(pdf, file=paste("Ust_Hght.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Ust_Hght.pdf2",sep=""), width = 10, height = 10)
 dev.off()
 
 #veg typeと臨界摩擦速度
@@ -69,7 +71,7 @@ boxplot(d$Ut~temp.type,xlab = "community type", ylab = "threshold friction veloc
      main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
 d.result <- lm(d$Ut ~ temp.type)
 summary(d.result)
-dev.copy(pdf, file=paste("Ust_Type.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Ust_Type.pdf2",sep=""), width = 10, height = 10)
 dev.off()
 
 #植生量と臨界摩擦速度
@@ -77,10 +79,10 @@ plot(d$DomVegVol,d$Ut,xlab = "vegetation volume(cm^2)", ylab = "threshold fricti
      main = "",cex.axis=1.2, cex.lab=1.5,cex.main = 1.5)
 d.result <- lm(d$Ut ~ d$DomVegVol)
 abline(d.result)
-text(3000,50,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
+text(45000,15,label = paste("y = ",round(d.result$coefficients[1],digits = 2), " + ",
                            round(d.result$coefficients[2],digits = 2),"x\nR^2: ", 
                            round(summary(d.result)$r.squared,digits = 2) ,sep = ""))
-dev.copy(pdf, file=paste("Ust_Vol.pdf",sep=""), width = 10, height = 10)
+dev.copy(pdf, file=paste("Ust_Vol.pdf2",sep=""), width = 10, height = 10)
 dev.off()
 
 library(MuMIn)
